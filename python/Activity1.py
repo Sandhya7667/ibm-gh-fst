cd67fa55a01ace51f68a335d8d12c7a8e2638dd4
Sai Sandhya <hsaisandhyareddy@gmail.com>
2026-06-23 09:35:52 +0530

    Add files via upload

---
+++ python/Activity1.py
@@ -0,0 +1,4 @@
+Name=input("enter your name:")
+Age=int(input("enter your age:"))
+year=2026+(100-Age)
+print("you will turn 100 by year:", year)
---
+++ python/Activity5.py
@@ -0,0 +1,2 @@
+for i in range(1, 11):
+    print("10 * " + str(i) + " = " + str(10 * i))
